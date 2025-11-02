d9dc55ec7a5b454aff05ad5550c8349e0f40584c
Khalid Khairkhwa <145394391+khalid64640@users.noreply.github.com>
2025-11-02 10:11:17 -0800

    Display item in a Grid

--- App.js
+++ App.js
@@ -1,36 +1,20 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View, Text } from "react-native";
-
-
+import CategoriesScreen from "./screens/CategoriesScreen";
 
 export default function App() {
-
-  // const [fontsloaded] = useFonts({
-  //   'bahij-nazanin': require('./assets/fonts/bahij-nazanin.ttf'),
-  //   'bahij-nazanin-bold': require('./assets/fonts/bahijbold.ttf')
-  // });
-
-  // if(!fontsloaded){
-  //   return <AppLoading />
-  // }
-
-
-
   return (
-    <View style = {styles.container}>
-      <Text>Hello World!!!!!</Text>
-      <StatusBar style="auto" /> 
-    </View>
+    <>
+      <StatusBar style="light"/>
+      <CategoriesScreen />
+    </>
   );
 }
 
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
+
   }
 
 });
--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -14,3 +14,6 @@ function  CategoriesScreen(){
 }
 
 export default CategoriesScreen;
+
+
+
