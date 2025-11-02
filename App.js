4a840a3c25cdb359df511b52b326f589b09b013a
Khalid Khairkhwa <145394391+khalid64640@users.noreply.github.com>
2025-11-02 08:18:55 -0800

    setup created

--- App.js
+++ App.js
@@ -1,66 +1,38 @@
-import { useState } from "react";
-import { StyleSheet, ImageBackground, SafeAreaView } from "react-native";
-import StartGameScreen from "./Screens/StartGameScreen";
-import { LinearGradient } from "expo-linear-gradient";
-import GameScreen from './Screens/GameScreen';
-import GameOverScreen from "./Screens/GameOverScreen";
-import { useFonts } from "expo-font";
-import AppLoading from "expo-app-loading";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View, Text } from "react-native";
+
+
 
 export default function App() {
-  const [userNumber, setUserNumber] = useState();
-  const [gameIsOver, setGameIsOver] = useState(true);
 
-  const [fontsloaded] = useFonts({
-    'bahij-nazanin': require('./assets/fonts/bahij-nazanin.ttf'),
-    'bahij-nazanin-bold': require('./assets/fonts/bahijbold.ttf')
-  });
+  // const [fontsloaded] = useFonts({
+  //   'bahij-nazanin': require('./assets/fonts/bahij-nazanin.ttf'),
+  //   'bahij-nazanin-bold': require('./assets/fonts/bahijbold.ttf')
+  // });
 
-  if(!fontsloaded){
-    return <AppLoading />
-  }
+  // if(!fontsloaded){
+  //   return <AppLoading />
+  // }
 
-  function pickedNumberHandler(pickedNumber) {
-    setUserNumber(pickedNumber);
-    setGameIsOver(false);
-  }
-  function GameOverHandler() {
-    setGameIsOver(true);
-  }
-
-  let screen = <StartGameScreen onPickNumber={pickedNumberHandler} />;
-  if (userNumber) {
-    screen = <GameScreen userNumber={userNumber} onGameOver = {GameOverHandler} />;
-  }
-
-  if (gameIsOver && userNumber) {
-    screen = <GameOverScreen />
-  }
 
 
   return (
-    <LinearGradient colors={['#4e0329', '#ddb52f']} style={styles.rootScreen} >
-      <ImageBackground
-        source={require('./assets/Background Image/background.png')}
-        resizeMethod="cover"
-        style={styles.rootScreen}
-        imageStyle={styles.backgroundImage}
-      >
-        <SafeAreaView style={styles.rootScreen}>{screen}</SafeAreaView>
-      </ImageBackground>
-    </LinearGradient>
+    <View style = {styles.container}>
+      <Text>Hello World!!!!!</Text>
+      <StatusBar style="auto" /> 
+    </View>
   );
 }
 
 
 const styles = StyleSheet.create({
-  rootScreen: {
+  container: {
     flex: 1,
-    backgroundColor: '#ddb52f',
-  },
-  backgroundImage: {
-    opacity: 0.15
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
   }
+
 });
 
 
--- Screens/GameOverScreen.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Text } from 'react-native';
-
-export default function GameOverScreen(){
-    return (
-        <Text>Game Over !</Text>
-    )
-}
-
